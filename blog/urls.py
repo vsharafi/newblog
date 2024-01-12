7d0e6782646290cefe91524b0824f3833cfe248b
vahid <vahid.sh1431@yahoo.com>
2024-01-12 18:41:23 +0330

    create , update and delete feature has been added

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     
     path('', views.PostListView, name='posts_list' ),
-    path('<int:pk>/', views.PostDetailView, name='post_detail')
+    path('<int:pk>/', views.PostDetailView, name='post_detail'),
+    path('add/', views.PostCreateView, name="post_add"),
+    path('<int:pk>/update/', views.PostUpdateView, name='post_update'),
+    path('<int:pk>/delete/', views.PostDeleteView, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
+
 
 
 def PostListView(request):
@@ -10,3 +12,32 @@ def PostListView(request):
 def PostDetailView(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def PostCreateView(request):
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_add.html', {'form': form})
+
+
+def PostUpdateView(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or None, instance=post )
+    if form.is_valid() and form.has_changed():
+        form.save()
+        return redirect('post_detail', pk=post.id)
+    return render(request, 'blog/post_add.html', {'form':form})
+
+
+def PostDeleteView(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('posts_list')
+    return render(request, 'blog/post_delete.html', {'post':post})
